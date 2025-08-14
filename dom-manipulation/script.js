50ba5b1d431e138793f681f9e6cd9dfd6388512a
Aola-j <okunolaabe@gmail.com>
2025-08-13 20:50:41 -0500

    dom Added

---
+++ dom-manipulation/script.js
@@ -0,0 +1,38 @@
+const quoteDisplay = document.getElementById("quoteDisplay")
+const newQuote = document.getElementById("newQuote")
+
+let quotes = [
+  { text: "The best way to get started is to quit talking and begin doing.", category: "inspiration" },
+  { text: "Don’t let yesterday take up too much of today.", category: "inspiration" },
+  { text: "I'm not arguing, I'm just explaining why I'm right.", category: "humor" },
+  { text: "I'm on a seafood diet. I see food, and I eat it.", category: "humor" }
+];
+
+function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
+  quoteDisplay.innerText = `"${quote.text}" — (${quote.category})`;
+}
+
+newQuote.addEventListener("click", showRandomQuote);
+
+
+function addQuote() {
+  const textInput = document.getElementById("newQuoteText");
+  const categoryInput = document.getElementById("newQuoteCategory");
+
+  const newText = textInput.value;
+  const newCategory = categoryInput.value;
+
+  if (newText && newCategory){
+    quotes.push({ text: newText, category: newCategory});
+
+    textInput.value = ""
+    categoryInput.value = ""
+
+    alert("Quote added successfully!")
+  } else {
+    alert ("please enter both quote and category")
+  }
+}
+
